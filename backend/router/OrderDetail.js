90e9a1af2b78c7bea066253830022de570127a4d
hdviux <hdvu2k@gmail.com>
2022-04-03 11:19:25 +0700

    update

--- backend/router/OrderDetail.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const OrderDetailController = require("../controllers/OrderDetail");
-const verifyToken = require("../middleware/auth");
-router.post("/addorderdetail",verifyToken.verifyToken,OrderDetailController.AddOrderDetail);
-router.put("/updateorderdetail/:orderDetailID",verifyToken.verifyToken,OrderDetailController.UpdateOrderDetail);
-module.exports = router;
